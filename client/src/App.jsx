52b58c2796487fa7cfbc57985f8d71b9fb9e59cc
Gaurav Chhimpa <chhimpagaurav21@gmail.com>
2026-04-10 15:22:33 +0530

    BackgroundImage Issue Resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,12 +24,12 @@ const App = () => {
 
 
       <Routes>
-        <Route path='/' element={Home} />
-        <Route path='/movies' element={Movies} />
-        <Route path='/movies/:id' element={MovieDetails} />
-        <Route path='/movies/:id/:date' element={SeatLayout} />
-        <Route path='/my-bookings' element={MyBooking} />
-        <Route path='/favorite' element={Favorite} />
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBooking />} />
+        <Route path='/favorite' element={<Favorite />} />
       </Routes>
 
 
