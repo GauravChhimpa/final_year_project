07adca4d66e4407489d8e892f1b620840f982929
Gaurav Chhimpa <chhimpagaurav21@gmail.com>
2026-04-12 21:35:44 +0530

    More Pages completed

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ const MovieCard = ({ movie }) => {
     const navigate = useNavigate()
 
   return (
-    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:translate-y-1 transition duration-300 w-64'>
+    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:translate-y-1 transition duration-300 w-56'>
 
         <img onClick={()=>{navigate(`/movies/${movie._id}`); scrollTo(0,0)}}
          src={movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-bottom-right cursor-pointer' />
